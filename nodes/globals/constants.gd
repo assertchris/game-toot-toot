89a50ac3061d8a25eb1c4dc6bb9c41d23b70d9d1
Christopher Pitt <cgpitt@gmail.com>
2022-04-02 14:52:27 +0200

    Initial commit

---
+++ nodes/globals/constants.gd
@@ -0,0 +1,4 @@
+extends Node
+
+const turn_color_closed := Color("#ff000280")
+const turn_color_open := Color("#00ff3680")
---
+++ nodes/vehicles/vehicle.gd
@@ -0,0 +1,27 @@
+extends PathFollow2D
+class_name GameVehicle
+
+signal faded_in
+signal faded_out
+
+@export var speed := 30
+
+@onready var _sprite := $Sprite
+@onready var _particles := $Particles
+@onready var _animator := $Animator
+
+func flip() -> void:
+	_animator.play("flip")
+
+func fade_in() -> void:
+	_animator.play("fade_in")
+	await _animator.animation_finished
+	faded_in.emit()
+
+func fade_out() -> void:
+	_animator.play_backwards("fade_in")
+	await _animator.animation_finished
+	faded_out.emit()
+
+func flip_sprite() -> void:
+	_sprite.flip_h = not _sprite.flip_h
